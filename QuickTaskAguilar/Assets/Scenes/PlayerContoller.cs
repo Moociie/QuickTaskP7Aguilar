708a0d30b4916cf90775906b865dc9668f68eefa
Kimberly Aguilar <757379@student.dusd.net>
2024-08-26 15:27:40 -0700

    Added movement
    need to add game over screen and smooth movement

--- QuickTaskAguilar/Assets/Scenes/PlayerContoller.cs
+++ QuickTaskAguilar/Assets/Scenes/PlayerContoller.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float moveSpeed;
+  
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
- 
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
     }
+   
 }
